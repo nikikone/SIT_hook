fdc9bac36191498c9dd5d7cc1c7c71f074dc6e05
nikikone <nikitaz0614@gmail.com>
2022-01-31 02:40:58 +1000

    [1] text_message

--- 123.py
+++ 123.py
@@ -1,17 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+import os
+print("1223", "2"
 
-print("1223", "2")
 
 
 
+
+
+
+
+)
 
 
 class File(models.Model):
     file = models.FileField(blank=False, null=False)
     remark = models.CharField(max_length=40)
     timestamp = models.DateTimeField(auto_now_add=True)
-
 
 
 
@@ -35,8 +40,6 @@ class ToDo(models.Model):
 
 
 
-
-
 class userProfile(models.Model):
     user = models.OneToOneField(
         User,
@@ -50,9 +53,6 @@ class userProfile(models.Model):
 
     def __str__(self):
         return self.user.username
-
-
-
 
 
 # Create your models here.
