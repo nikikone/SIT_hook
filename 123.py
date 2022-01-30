0e5e0d459793461b5d3b26deb0927c7221159260
nikikone <nikitaz0614@gmail.com>
2022-01-31 03:35:15 +1000

    [1] uploading

--- 123.py
+++ 123.py
@@ -1,12 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-print("1223", "2"
 
-
-
-
-
-      )
+print("1223", "2")
 
 
 class File(models.Model):
@@ -32,10 +27,7 @@ class ToDo(models.Model):
 
 
 class userProfile(models.Model):
-    user = models.OneToOneField(
-        User,
-        on_delete=models.CASCADE,
-        related_name="profile")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     description = models.TextField(blank=True, null=True)
     location = models.CharField(max_length=30, blank=True)
     date_joined = models.DateTimeField(auto_now_add=True)
