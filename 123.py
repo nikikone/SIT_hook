c67bc3816f085a9b2468ff0d9c4225f5a95ee8f2
nikikone <nikitaz0614@gmail.com>
2022-01-31 00:51:49 +1000

    [1] 1234

--- 123.py
+++ 123.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-phtc = 123
+123
 class File(models.Model):
     file = models.FileField(blank=False, null=False)
     remark = models.CharField(max_length=40)
