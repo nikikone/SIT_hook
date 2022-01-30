0d4c6ed8a11ccbac96475f7af9216e7f51cceffb
nikikone <nikitaz0614@gmail.com>
2022-01-31 01:13:23 +1000

    [1] 213

--- 123.py
+++ 123.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-print("1223", "2"               )
+print("1223", "2"                             )
 
 
 class File(models.Model):
